65d33e6594f6db96a21e466420e71b5191a2c34b
nadamalash <nadamalash58@gmail.com>
2023-02-23 10:50:17 +0200

    Create Signup Activity First Version Needed To Enhance Scrollview

--- app/src/main/java/com/malash/shopapp/activities/LoginActivity.kt
+++ app/src/main/java/com/malash/shopapp/activities/LoginActivity.kt
@@ -1,7 +1,9 @@
 package com.malash.shopapp.activities
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import com.malash.shopapp.R
 
 class LoginActivity : AppCompatActivity() {
@@ -9,5 +11,10 @@ class LoginActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
+        //Just for test
+        val login = findViewById<Button>(R.id.btn_login)
+        login.setOnClickListener {
+            startActivity(Intent(this, SignupActivity::class.java))
+        }
     }
 }
---
+++ app/src/main/java/com/malash/shopapp/activities/SignupActivity.kt
@@ -0,0 +1,12 @@
+package com.malash.shopapp.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.malash.shopapp.R
+
+class SignupActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_signup)
+    }
+}
